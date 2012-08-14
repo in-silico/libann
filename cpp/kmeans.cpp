f09c0ee6b48f64d5dcf68deb70ca4d2460e712e4
insilico <insilico@sebasPC.utp.edu.co>
2012-08-14 16:56:19 -0500

    implementation of findClosestCentroids

--- cpp/kmeans.cpp
+++ cpp/kmeans.cpp
@@ -1,5 +1,9 @@
 
 #include "kmeans.h"
+#include <limits>
+#define rep(i,n) for(int i=0; i<(n); i++)
+
+const double inf = std::numeric_limits<double>::max(); 
 
 using namespace LibAnn;
 
@@ -17,7 +21,34 @@ void kmeansInit(Mat *centroids, const Mat *x, int k) {
 void normalize(Mat *dest, const Mat *org) {
 }
 
-void findClosestCentroids(int *idx, const Mat *x, const Mat *centroids) {
+
+/**
+    Computes the N-dimentional "distance" 
+*/
+double distance(Mat *a, Mat *b,const int a_row,const int b_row){
+    if(a->ncols()!=b->ncols())
+        throw "The vectors have different dimensions";
+    double acum=1;
+    rep(j,a->ncols()){
+        acum*= (a->get(a_row,j) - b->get(b_row,j) );
+    }
+    return acum;
+}
+
+void findClosestCentroids(int *idx, Mat *x, Mat *centroids) {
+    int x_r=x->nrows() , cent_r= centroids->nrows(), row_min=0;
+    double m,tmp;
+    rep(i,x_r){
+        m = inf;
+        rep(j,cent_r){
+            tmp = distance(x,centroids,i,j);
+            if(tmp<m){
+                m=tmp;
+                row_min=j;    
+            }
+        }
+        idx[i]=row_min;
+    }
 }
 
 void computeCentroids(Mat *centroids, const Mat* x, int *idx, int k) {
