e84b4a6cec712d79f00a1b8f7763c9f5e5bf0a6d
Alejandro <ing.alsuga@gmail.com>
2012-08-14 16:41:07 -0500

    std
    mean and standar deviation

--- cpp/kmeans.cpp
+++ cpp/kmeans.cpp
@@ -1,4 +1,4 @@
-
+#include<cmath>
 #include "kmeans.h"
 
 using namespace LibAnn;
@@ -14,6 +14,26 @@ int kmeans(Mat *centers, const Mat *x, const Mat *initial_centroids, int maxIter
 void kmeansInit(Mat *centroids, const Mat *x, int k) {
 }
 
+void std(Mat *stdev, Mat *mean, Mat *x){
+    double tmp;
+    //mean
+    for(int j=0; j < x->ncols() ; j++){
+        tmp=0;
+        for(int i=0; i < x->nrows() ; i++){
+            tmp += x->get(i,j);
+        }
+        mean->get(0,j)= tmp/(x->nrows());
+    }
+    //standar deviation
+    for(int j=0; j < x->ncols() ; j++){
+        tmp=0;
+        for(int i=0; i < x->nrows() ; i++){
+            tmp += pow(x->get(i,j),2);
+        }
+        stdev->get(0,j)= (tmp/x->nrows()) - pow(mean->get(0,j),2);
+    }
+
+}
 void normalize(Mat *dest, const Mat *org) {
 }
 
