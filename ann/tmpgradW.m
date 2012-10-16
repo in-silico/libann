3f233e05d2089cafbbbc34a1e204891caf348210
sebasutp <sebasutp@gmail.com>
2012-10-15 21:01:37 -0500

    Version with cpp gradients is working correctly

--- ann/tmpgradW.m
+++ ann/tmpgradW.m
@@ -3,17 +3,16 @@ function deltaW = tmpgradW(Y,R,Z,V,Xt)
     H = size(V,2)-1;
     D = size(Xt,2)-1;
     deltaW = zeros(H,D+1);
-    for h = 1:H
-	for j = 1:D+1
-	    for t = 1:N
+    for t = 1:N
+      for h = 1:H
+	for j = 1:D+1	    
 		tmp=0;
 		for i = 1:K
 		    tmp = tmp + (R(t,i) - Y(t,i))*V(i,h+1);
 		end
 		tmp2 = Z(t,h)*(1-Z(t,h))*Xt(t,j);
-		deltaW(h,j) += tmp*tmp2;
-	    end
+		deltaW(h,j) -= tmp*tmp2;	    
 	end
+      end
     end
-    deltaW = deltaW*-1;
 end
