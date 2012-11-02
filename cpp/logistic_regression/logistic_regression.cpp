5299ee15b0032da791d2d8f3049068071a9f49af
pin3da <mauel.felipe.pineda@gmail.com>
2012-11-02 15:26:11 -0500

    code ligistic_regression

--- cpp/logistic_regression/logistic_regression.cpp
+++ cpp/logistic_regression/logistic_regression.cpp
@@ -1,4 +1,50 @@
 #include "logistic_regression.h"
 
 
+namespace LAnn{
+    void sigmoid(Mat *z, Mat *g){
 
+
+    }
+
+    void sigmoidGradient(Mat *z,Mat *g){
+
+
+    }
+
+    void regerrnn(NeuronalN nn, Mat *x, Mat *R, Mat *E){
+
+
+    }
+
+    void classerrnn(NeuronalN nn, Mat *x, Mat *R, Mat *E){
+
+
+    }
+
+    void regnn(NeuronalN nn, Mat *x, Mat *y, Mat *z){
+
+    }
+
+    void nntrain(NeuronalN nn, Mat *x, Mat *r, int iter, void (compnn *)(NeuronalN nn, Mat *x, Mat *y, Mat *z), \
+                     void (* errnn)(NeuronalN nn,Mat *x, Mat *R, Mat *E)){
+                     
+                     
+    }
+
+    void nngradV(Mat *y, Mat *r,Mat *Zt, Mat *v, Mat *dv){
+
+    }
+
+    void nngradW(Mat *y, Mat *r,Mat *Z, Mat *v,Mat *xt, Mat *dw){
+
+
+    }
+
+    void nngrad(NeuronalN nn,Mat *wv, Mat *x, Mat *r, void (compnn *)(NeuronalN nn, Mat *x, Mat *y, Mat *z), \
+                     void (* errnn)(NeuronalN nn,Mat *x, Mat *R, Mat *E),Mat * E,Mat *grad){
+                     
+                     
+                     
+    }
+}
