fbd411231c6f4da238053efcdf88402a0c095ed2
sebas <sebasutp@gmail.com>
2013-05-24 15:48:18 -0400

    img_lib methods

--- vision/cpp_version/img_lib.h
+++ vision/cpp_version/img_lib.h
@@ -1,11 +1,19 @@
 
+#ifndef IMG_LIB_H
+#define IMG_LIB_H
+
 #include <cv.h>
 #include <highgui.h>
+#include <string>
+
+using namespace std;
 
 template<class T>
 class Image {
     T *data;
     int rows,cols;
+    
+    T &at(int i, int j);
 public:
     Image(int row, int cols);
     ~Image();
@@ -14,6 +22,15 @@ public:
     void convolve(Image &kernel);
     void operator=(Image &other);
     void normalize(int minVal=0, int maxVal=255);
+};
+
+struct ImgError {
+    int code;
+    string message;
+
+    ImgError(int code, const char *error) {
+        this->code = code; message = string(error);
+    }
 };
 
 template<class T>
@@ -33,3 +50,20 @@ void img2mat(IplImage *ocv, Image<T> *img);
 
 template<class T>
 void mat2img(Image<T> *img, IplImage *ocv);
+
+template<class T>
+T& Image<T>::at(int i, int j) {
+    if (i<0) i=0;
+    if (j<0) j=0;
+    if (i>=rows) i=rows-1;
+    if (j>=cols) j=cols-1;
+    return data[i*rows + j];
+}
+
+template<class T>
+T& Image<T>::operator()(int i, int j) {
+    if (i<0 || j<0 || i>=rows || j>=cols) throw ImgError(1,"Out of bound index exception");
+    return data[i*rows + j];
+}
+
+#endif
