d72ef79274450eadf23c014fad0ccdfa5707e6c3
Sebastián Gómez <sebasutp@gmail.com>
2013-06-02 10:52:48 -0500

    Slow image matching tested and working OK

--- vision/cpp_version/img_test.cpp
+++ vision/cpp_version/img_test.cpp
@@ -84,31 +84,57 @@ void cornerTest(Image<T> &img2, Image<T> &img) {
 }
 
 template<class T>
-void correspTest(Image<T> &ans, Image<T> &img1, Image<T> &img2) {
+void correspTest(Image<T> &img1, Image<T> &img2) {
     corresp pts; CorrespParams params;
-    params.matchThresh=-20;
-    params.nmsThresh=1e8;
+    params.matchThresh=-200000;
+    params.nmsThresh=1e10;
+    params.scoreW = 11;
     findCorrespondences(pts, img1, img2, params);
+
+    //create image with two images left and right
+    int n = img1.rows; //assumed equal to img2.rows
+    int m = img1.cols + img2.cols + 1;
+    IplImage *cvg = cvCreateImage(cvSize(m,n),IPL_DEPTH_8U,1);
+    for (int i=0; i<n; i++) {
+        uchar *row = (uchar*)(cvg->imageData + i*cvg->widthStep);
+        T *r1 = img1.getRow(i); T *r2 = img2.getRow(i);
+        for (int j=0; j<img1.cols; j++) row[j] = (uchar)r1[j];
+        row[img1.cols] = 0; //black line in the middle
+        for (int j=0; j<img2.cols; j++) row[img1.cols + j + 1] = (uchar)r2[j];
+    }
+
+    double p = 0.2; //probability that a correspondence is shown
     for (int i=0; i<pts.size(); i++) {
+        double r = (rand() / ((double)RAND_MAX));
         ipt p1 = pts[i].first, p2 = pts[i].second;
+        if (r<p) {
+            cvCircle(cvg,cvPoint(p1.y,p1.x),3,cvScalar(0,0,0));
+            cvCircle(cvg,cvPoint(p2.y+img1.cols+1,p2.x),3,cvScalar(0,0,0));
+            cvLine(cvg,cvPoint(p1.y,p1.x),cvPoint(p2.y +img1.cols + 1,p2.x),cvScalar(0,0,0));
+        }
         printf("(%d, %d) -> (%d, %d)\n",p1.x,p1.y,p2.x,p2.y);
     }
+    cvSaveImage("out.jpg",cvg);
     printf("Done\n");
 }
 
 void test1() {
-    IplImage *cvi = cvLoadImage("test.jpg");
+    IplImage *cvi = cvLoadImage("left.jpg");
     int m = cvi->width, n = cvi->height;
     IplImage *cvg = cvCreateImage(cvSize(m,n),IPL_DEPTH_8U,1);
     cvCvtColor(cvi,cvg,CV_RGB2GRAY);
-    Image<float> img1(n,m);
-    Image<float> img2(n,m);
-    Image<float> ans(n,m);
-    cvimg2img(img1, cvg); cvimg2img(img2, cvg);
-    
-    cornerTest(ans, img1);
-    correspTest(ans,img1,img2);
-    saveImg("test2.jpg", ans);
+    Image<float> img1(n,m); cvimg2img(img1, cvg); 
+
+    cvi = cvLoadImage("right.jpg");
+    m = cvi->width, n = cvi->height;
+    cvg = cvCreateImage(cvSize(m,n),IPL_DEPTH_8U,1);
+    cvCvtColor(cvi,cvg,CV_RGB2GRAY);
+    Image<float> img2(n,m); cvimg2img(img2, cvg);
+
+    //Image<float> ans(n,2*m);    
+    //cornerTest(ans, img1);
+    correspTest(img1,img2);
+    //saveImg("test2.jpg", ans);
     cvReleaseImage( &cvi ); cvReleaseImage( &cvg );
 }
 
@@ -132,6 +158,7 @@ void test2(){
 }
 
 int main() {
+    srand( time(0) );
     try{
         test1();
         //test2();    
