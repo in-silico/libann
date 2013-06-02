0bdae9f74c06754c53baeed357f67a35d8baaaa5
pin3da <manuel.felipe.pineda@gmail.com>
2013-06-02 18:52:06 -0500

    Added script for compilation troubles

--- vision/cpp_version/build_all.sh
+++ vision/cpp_version/build_all.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 if [ $# -gt 0 ] ; then
-	base=`basename $1 .c`
+	base=`basename $1 .cpp`
 	echo "compiling $base"
-	gcc -ggdb `pkg-config opencv --cflags --libs` $base.c -o $base 
+	gcc -ggdb `pkg-config opencv --cflags --libs` $base.cpp -o $base -O2
 else
 	for i in *.c; do
 	    echo "compiling $i"
-	    gcc -ggdb `pkg-config --cflags opencv` -o `basename $i .c` $i `pkg-config --libs opencv`;
+	    gcc -ggdb `pkg-config --cflags opencv` -o `basename $i .c` $i `pkg-config --libs opencv` -O2;
 	done
 	for i in *.cpp; do
 	    echo "compiling $i"
-	    g++ -ggdb `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
+	    g++ -ggdb `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv` -O2;
 	done
 fi
